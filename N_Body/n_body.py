f5a11a36fbf7804f0c6f2fb95382f3521322a85c
pschassberger <pschassberger@gmail.com>
2020-07-22 15:55:26 -0600

    updates

--- N_Body/n_body.py
+++ N_Body/n_body.py
@@ -1,5 +1,12 @@
 '''
 This program will be a simulation of the N_Body problem 
 according to classical mechanics. 
+F = m*a
+m(dvx/dt) = -GMmx / r**3
+m(dvy/dt) = -GMmy / r**3
+r = sqrt(x**2 + y**2)
 '''
+import numpy as np
+import matplotlib 
+
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Math
 These programs are mathematical theorems and physics problems. 
-Will Update as we go Along
+Will Update as we go along.
 
 Mandelbrot Set
 
