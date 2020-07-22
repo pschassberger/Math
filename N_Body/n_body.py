477849f79ffb03c08006117c435a584c895982a3
pschassberger <pschassberger@gmail.com>
2020-07-22 16:05:37 -0600

    added constants

--- N_Body/n_body.py
+++ N_Body/n_body.py
@@ -4,9 +4,19 @@ according to classical mechanics.
 F = m*a
 m(dvx/dt) = -GMmx / r**3
 m(dvy/dt) = -GMmy / r**3
-r = sqrt(x**2 + y**2)
+m(dvz/dt) = -GMmz / r**3
+r = sqrt(x**2 + y**2 + z**2)
 '''
 import numpy as np
-import matplotlib 
+import matplotlib as plt
+
+# Constants in SI
+GC = 6.673e-11
+M_SUN = 1.989e30 
+M_EARTH = 5.972e24
+M_JUPITER = 7.898e27
+AU = 150e6 
+
+
 
 
