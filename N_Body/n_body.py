bug in calculation

N_Body/n_body.py
@@ -24,15 +24,46 @@ dt = 0.1
 t = 0.0
 x = 0.5
 y = 0.0
-v_x = 0.0
-v_y = 1.63
+vy = 1.630
+vx = 0.0
 
 def calculations(time):
     radii = np.sqrt(x**2 + y**2)
     # acceleration
     a_x = -x / radii**3
+    a_y = -y / radii**3
+    r_cube = 1 / radii**3
+    # store coordinates
+    x_pos=[]
+    y_pos=[]
+    radii_list=[]
+    r_cube_list=[]
+    ax_list=[]
+    ay_list=[]
+    # velocities
+    v_x = vx + a_x * radii
+    v_y = vy + a_y * radii
 
+    for i in range(time):
+        x1 = x + v_x * dt
+        y1 = y + v_y * dt
+        radii1 = np.sqrt(x1**2 + y1**2)
+        r_cube1 = 1 / radii1**3
+        a_x1 = -x1 / radii1**3
+        a_y1 = -y1 / radii1**3
 
+        x_pos.append(x1)
+        y_pos.append(y1)
+        radii_list.append(radii1)
+        r_cube_list.append(r_cube1)
+        ax_list.append(a_x1)
+        ay_list.append(a_y1)
+
+        dt += 0.1
+
+    print(x_pos, y_pos, radii_list, r_cube_list, ax_list, ay_list)
+
+calculations(10)
 
 
 