df1bd87c1194f49376798730f5bfe49e9b630752
pschassberger <pschassberger@gmail.com>
2020-08-05 17:04:26 -0600

    taking a break

--- N_Body/n_body.py
+++ N_Body/n_body.py
@@ -11,12 +11,12 @@ AU = 150e6
 
 # construct planets
 sun = Planet("Sun", M_SUN, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-earth = Planet("Earth", M_EARTH, AU, 0.0, 0.0, -0.2, 460, 0.0, -4.44e-17, 0.0, 0.0)
+earth = Planet("Earth", M_EARTH, AU, 0.0, 0.0, -1.918e-12, 1.63e1, 0.0, -4.44e-17, 0.0, 0.0)
 
 # calculation
 def calculations(time, planet_1, planet_2):
     # time increment
-    dt = 3600
+    dt = 3600 * 24
     radii_list=[]
     dr_radii = np.sqrt((planet_1.x_pos_list[0] - planet_2.x_pos_list[0])**2 
                         + (planet_1.y_pos_list[0] - planet_2.y_pos_list[0])**2
@@ -73,18 +73,18 @@ def calculations(time, planet_1, planet_2):
         
         # velocities
         # planet 1
-        dvx_pln1 = planet_1.x_vel_list[i] + planet_1.x_accel_list[i+1] * dt
+        dvx_pln1 = planet_1.x_vel_list[i] + planet_1.x_accel_list[i] * dt
         planet_1.x_vel_list.append(dvx_pln1)
-        dvy_pln1 = planet_1.y_vel_list[i] + planet_1.y_accel_list[i+1] * dt
+        dvy_pln1 = planet_1.y_vel_list[i] + planet_1.y_accel_list[i] * dt
         planet_1.y_vel_list.append(dvy_pln1)
-        dvz_pln1 = planet_1.z_vel_list[i] + planet_1.z_accel_list[i+1] * dt
+        dvz_pln1 = planet_1.z_vel_list[i] + planet_1.z_accel_list[i] * dt
         planet_1.z_vel_list.append(dvz_pln1)
         # planet 2
-        dvx_pln2 = planet_2.x_vel_list[i] + planet_2.x_accel_list[i+1] * dt
+        dvx_pln2 = planet_2.x_vel_list[i] + planet_2.x_accel_list[i] * dt
         planet_2.x_vel_list.append(dvx_pln2)
-        dvy_pln2 = planet_2.y_vel_list[i] + planet_2.y_accel_list[i+1] * dt
+        dvy_pln2 = planet_2.y_vel_list[i] + planet_2.y_accel_list[i] * dt
         planet_2.y_vel_list.append(dvy_pln2)
-        dvz_pln2 = planet_2.z_vel_list[i] + planet_2.z_accel_list[i+1] * dt
+        dvz_pln2 = planet_2.z_vel_list[i] + planet_2.z_accel_list[i] * dt
         planet_2.z_vel_list.append(dvz_pln2)
         
 
